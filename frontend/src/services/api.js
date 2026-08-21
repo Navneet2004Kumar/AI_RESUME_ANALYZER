06f7c0d84d823b54bd71d9f962848e64a57af6d8
Navneet <upadhyaynaveentkumar@gmail.com>
2026-08-21 13:53:47 +0530

    Fix API baseURL to use environment variable

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://127.0.0.1:5000",
+    baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:5000",
+
 });
 
 export const analyzeResume = async (formData) => {
